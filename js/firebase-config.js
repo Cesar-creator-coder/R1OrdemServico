f13dbfc1e680cc6680748ad2832e23e1e7a80b5f
Cesar-creator-coder <albertsilverios@gmail.com>
2026-08-15 13:44:45 -0300

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,12 @@
-
+//js/firebase-config.js
+// CÓDIGO ANALISADO E ATUALIZADO POR MIM. COPIE ISTO TUDO, CÉSAR:
+var firebaseConfig = {
+  apiKey: "AIzaSyCzz7FIZgB3EQVCw8CD4HQVMTehU11fXfg",
+  authDomain: "r1oservico-hub.firebaseapp.com",
+  projectId: "r1oservico-hub",
+  storageBucket: "r1oservico-hub.firebasestorage.app",
+  messagingSenderId: "1080136865646",
+  appId: "1:1080136865646:web:568dbfc6971054d0a8137f",
+  // EU ADICIONEI ESTA LINHA CRUCIAL PARA O BANCO DE DADOS:
+  databaseURL: "https://r1oservico-hub.firebaseio.com"
+};
