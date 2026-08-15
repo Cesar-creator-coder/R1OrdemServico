36e4eccd76d194d4f1028f95caf8dde580d892d2
Cesar-creator-coder <albertsilverios@gmail.com>
2026-08-15 15:43:56 -0300

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,4 @@
-//js/firebase-config.js
+// firebase-config.js
 // CÓDIGO ANALISADO E ATUALIZADO POR MIM. COPIE ISTO TUDO, CÉSAR:
 var firebaseConfig = {
   apiKey: "AIzaSyCzz7FIZgB3EQVCw8CD4HQVMTehU11fXfg",
@@ -6,7 +6,7 @@ var firebaseConfig = {
   projectId: "r1oservico-hub",
   storageBucket: "r1oservico-hub.firebasestorage.app",
   messagingSenderId: "1080136865646",
-  appId: "1:1080136865646:web:568dbfc6971054d0a8137f",
+  appId: "1:1080136865646:web:568dbfc6971054d08137f",
   // EU ADICIONEI ESTA LINHA CRUCIAL PARA O BANCO DE DADOS:
   databaseURL: "https://r1oservico-hub.firebaseio.com"
 };
